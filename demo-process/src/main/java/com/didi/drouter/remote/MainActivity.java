b16d3d4bade383d0cde329549a2ed727edac4408
gaowei <gwblue7@gmail.com>
2022-03-12 17:03:00 +0800

    split page-api module

--- demo-process/src/main/java/com/didi/drouter/remote/MainActivity.java
+++ demo-process/src/main/java/com/didi/drouter/remote/MainActivity.java
@@ -18,6 +18,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import com.didi.drouter.process.R;
 
 public class MainActivity extends AppCompatActivity {
     private IRemoteFunction remoteFunction;
